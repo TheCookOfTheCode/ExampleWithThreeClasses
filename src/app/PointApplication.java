2f8f44773e6d3ecd87bdfc4a6920c4f5530484ee
Przemysław Chmielewski <p.chmielewski91@o2.pl>
2018-02-04 22:45:05 +0100

    #02
    the switch has been added in the version  0.2

--- src/app/PointApplication.java
+++ src/app/PointApplication.java
@@ -5,20 +5,45 @@ import data.Point;
 
 public class PointApplication {
 
+	public static final int ADD_X = 0;
+	public static final int ADD_Y = 1;
+	public static final int MINUS_X = 2;
+	public static final int MINUS_Y = 3;
+
 	public static void main(String[] args) {
-		
-		Point p1 = new Point(10,20);
+		Point p1 = new Point(10, 20);
 		PointController pc = new PointController();
-		
-		System.out.println("Punkt: ("+p1.getX()+";"+p1.getY()+")");
+
+		System.out.println("Punkt przed zmian¹: (" + p1.getX() + ";" + p1.getY() + ")");
+
+		int option = 0;
+
+		switch (option) {
+		case ADD_X:
+			pc.addX(p1);
+			break;
+		case ADD_Y:
+			pc.addY(p1);
+			break;
+		case MINUS_X:
+			pc.minusX(p1);
+			break;
+		case MINUS_Y:
+			pc.minusY(p1);
+		default:
+			System.out.println("Podano b³êdn¹ wartoœæ");
+		}
+
+		System.out.println("Punkt po zmianie: (" + p1.getX() + ";" + p1.getY() + ")");
 		pc.addX(p1);
-		System.out.println("Punkt addX: ("+ p1.getX()+";"+p1.getY()+")");
+		System.out.println("Punkt addX: (" + p1.getX() + ";" + p1.getY() + ")");
 		pc.addY(p1);
-		System.out.println("Punkt addY: ("+ p1.getX()+";"+p1.getY()+")");
+		System.out.println("Punkt addY: (" + p1.getX() + ";" + p1.getY() + ")");
 		pc.minusX(p1);
-		System.out.println("Punkt minusX: ("+ p1.getX()+";"+p1.getY()+")");
+		System.out.println("Punkt minusX: (" + p1.getX() + ";" + p1.getY() + ")");
 		pc.minusY(p1);
-		System.out.println("Punkt minusY: ("+ p1.getX()+";"+p1.getY()+")");
+		System.out.println("Punkt minusY: (" + p1.getX() + ";" + p1.getY() + ")");
+
 	}
 
 }
